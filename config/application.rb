26516d272833c9ad570f9287fd58e0bd63355791
webui7 <webui7@gmail.com>
2013-04-23 02:34:10 +0300

    'bnb'

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,8 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require "devise"
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
