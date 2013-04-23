93f255302e69879bdc8eeba59c74d2735ed5cacf
webui7 <webui7@gmail.com>
2013-04-23 10:45:27 +0300

    'bnb'

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,5 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
-required 'devise'
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
